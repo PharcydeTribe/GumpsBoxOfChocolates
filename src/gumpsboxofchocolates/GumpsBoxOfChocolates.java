dcb42fb5a9870f3646d952343299ca386e2df84f
Steven Kritikos <stevenrkritikos@outlook.com>
2017-02-07 21:24:10 -0500

    Added ez to use truth checker

--- src/gumpsboxofchocolates/GumpsBoxOfChocolates.java
+++ src/gumpsboxofchocolates/GumpsBoxOfChocolates.java
@@ -1,8 +1,15 @@
 package gumpsboxofchocolates;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class GumpsBoxOfChocolates {
     public static void main(String[] args) {    
         System.out.println("Hello " + (Math.floor(Math.random()*2)<1?"Steven":"Στέλιος"));
         int x=0;int y=0;int GRID_DIM=5;
         do {System.out.println("row="+y+",col="+x);} while ((++x>GRID_DIM?x=(++y>GRID_DIM?x:0):x)<=GRID_DIM);
+        if(Sanity.check()){System.out.println("true is true");}else{try {throw new AnyException();}catch(AnyException ex){System.err.print(ex.getStackTrace());};}
     }
 }
+
+
---
+++ src/gumpsboxofchocolates/Sanity.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package gumpsboxofchocolates;
+
+/**
+ *
+ * @author Steven Kritikos
+ */
+public class Sanity {
+  static boolean check() {
+    return false!=true&&false==false&&true==true?true:Sanity.check();
+  }
+}
