6b5ebadea5bc51ab6eac3542b42c124ace1625e6
Aiden <Aiden@Occam-Redux>
2017-02-07 20:20:02 -0500

    Dirtiest do{}while() this side of the Mississippi

--- src/gumpsboxofchocolates/GumpsBoxOfChocolates.java
+++ src/gumpsboxofchocolates/GumpsBoxOfChocolates.java
@@ -2,5 +2,7 @@ package gumpsboxofchocolates;
 public class GumpsBoxOfChocolates {
     public static void main(String[] args) {    
         System.out.println("Hello " + (Math.floor(Math.random()*2)<1?"Steven":"Στέλιος"));
+        int x=0;int y=0;int GRID_DIM=5;
+        do {System.out.println("row="+y+",col="+x);} while ((++x>GRID_DIM?x=(++y>GRID_DIM?x:0):x)<=GRID_DIM);
     }
 }
